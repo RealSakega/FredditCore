11a5f2620546ec9d1be7e595dce38a873269f56b
FredditFreebuild <147449205+OperationalOverseer@users.noreply.github.com>
2023-10-12 22:28:15 +0200

    backup script

--- scripts/BACKUP_FREEBUILD.sh
+++ scripts/BACKUP_FREEBUILD.sh
@@ -6,12 +6,12 @@ source ../.secrets/webhook_urls.sh
 
 cd ../minecraft/backups/Freebuild
 ls -1t | tail -n +7 | xargs rm
-cd ..
+cd ../..
 
 post_status () {
     curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$1\"}" $BACKUPS_STAFF_CHANNEL_WEBHOOK
 }
 
 post_status "Creating backup of reality FRFR-25565."
-zip -r "Freebuild/$(date +"%Y-%m-%d-%H-%M").zip" $(cat BackupList_Freebuild.txt)
+zip -r "minecraft/backups/Freebuild/$(date +"%Y-%m-%d-%H-%M").zip" $(cat minecraft/backups/BackupList_Freebuild.txt)
 post_status "Backup completed"
