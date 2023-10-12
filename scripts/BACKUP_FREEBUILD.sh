407b2b53c6aeec31588e9f8702193f4f4d021741
FredditFreebuild <147449205+OperationalOverseer@users.noreply.github.com>
2023-10-12 22:29:19 +0200

    Update BACKUP_FREEBUILD.sh

--- scripts/BACKUP_FREEBUILD.sh
+++ scripts/BACKUP_FREEBUILD.sh
@@ -6,7 +6,7 @@ source ../.secrets/webhook_urls.sh
 
 cd ../minecraft/backups/Freebuild
 ls -1t | tail -n +7 | xargs rm
-cd ../..
+cd ../../..
 
 post_status () {
     curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$1\"}" $BACKUPS_STAFF_CHANNEL_WEBHOOK
