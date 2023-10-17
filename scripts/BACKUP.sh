d15bff05bc36d5f5bc71d24d156dff6b297a0d83
FredditFreebuild <147449205+OperationalOverseer@users.noreply.github.com>
2023-10-17 23:52:43 +0200

    Update BACKUP.sh

--- scripts/BACKUP.sh
+++ scripts/BACKUP.sh
@@ -23,16 +23,21 @@ if [ -v BACKUPS_STAFF_CHANNEL_WEBHOOK ]; then
         curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$1\"}" $BACKUPS_STAFF_CHANNEL_WEBHOOK
     }
 
-    backup_output_file = "${target_dir}/${zipname}"
-
-    echo $backup_output_file
+    backup_output_file="${target_dir}/${zipname}"
 
     post_status "Creating reality backup. (-> `${backup_output_file}`)"
-    zip -r "${backup_output_file}" $(cat ${backup_list})
-    post_status "Backup completed"
+    if zip -r "${backup_output_file}" $(cat ${backup_list}); then
+        post_status "Backup completed"
+    else
+        post_status "Backup failed"
+    fi
 else 
     echo "BACKUPS_STAFF_CHANNEL_WEBHOOK is not set"
-    zip -r "${target_dir}/${zipname}" $(cat ${backup_list})
+    if zip -r "${target_dir}/${zipname}" $(cat ${backup_list}); then
+        echo "Backup completed"
+    else
+        echo "Backup failed"
+    fi
 fi
 
 exit 0
