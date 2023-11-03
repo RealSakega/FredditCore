f6ab3c0a49205b0ffb3cef76a216322f2b2c7b48
OperationalOverseer <operationaloverseer@gmail.com>
2023-11-03 12:50:14 +0100

    d

--- scripts/SERVER_STATUS_TO_WEBHOOK.sh
+++ scripts/SERVER_STATUS_TO_WEBHOOK.sh
@@ -17,12 +17,11 @@ post_status () {
 # https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
 server_status="$(
   date
-  free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
-  df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
+  # top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' 
+  free -m | awk 'NR==2{printf "- Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
+  df -h | awk '$NF=="/"{printf "- Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
   echo "Active processes:"
   screen -ls | grep Detached | awk '{print "- " $1 " " $2 " " $3}'
-
-  # top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' 
 )"
 
 post_status "${server_status@Q}"
