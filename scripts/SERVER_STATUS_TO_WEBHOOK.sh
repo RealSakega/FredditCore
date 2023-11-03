7e0b95e3327ed891120f14b7fcd661194d7f2fc1
OperationalOverseer <operationaloverseer@gmail.com>
2023-11-03 12:47:50 +0100

    screens

--- scripts/SERVER_STATUS_TO_WEBHOOK.sh
+++ scripts/SERVER_STATUS_TO_WEBHOOK.sh
@@ -19,6 +19,9 @@ server_status="$(
   date
   free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
   df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}'
+  echo "Active processes:"
+  screen -ls | grep Detached | awk '{print " - " $1 " " $2 " " $3}'
+
   # top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' 
 )"
 
