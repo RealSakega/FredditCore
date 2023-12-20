75f4b73e959d381ae0950e163723ec930e9939ac
orbitaldot <orbitaldotogg@gmail.com>
2023-12-21 00:44:12 +0100

    Update server.js

--- website/server.js
+++ website/server.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 8008;
 
 app.use(express.static(__dirname + '/public'))
 
-// sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
