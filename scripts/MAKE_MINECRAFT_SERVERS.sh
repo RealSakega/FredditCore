efa9962606d4e8d3d6e9d06474421a800b2e7c48
FredditFreebuild <FredditTechSusport@gmail.com>
2024-02-08 15:59:39 +0100

    docu

--- scripts/MAKE_MINECRAFT_SERVERS.sh
+++ scripts/MAKE_MINECRAFT_SERVERS.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 
+# This script takes a Make target as an argument, and then
+# executes it for all Minecraft servers.
+#
+# For example, `bash MAKE_MINECRAFT_SERVERS.sh backup` would
+# execute `make backup` for all of the Minecraft servers.
+
 if [ $# -lt 1 ]; then
     echo "Usage: $0 make_target"
     exit 1
 fi
 
 cd "$(dirname "$0")"
-
 cd ../minecraft/servers
 
 for server in $(ls | grep -v shared); do
@@ -14,3 +19,5 @@ for server in $(ls | grep -v shared); do
     make $1
     cd ..
 done
+exit 0
+
