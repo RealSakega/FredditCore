b457060c0d689aca6bb7e1cc20442abd10fcdbcf
orbitaldot <orbitaldotogg@gmail.com>
2024-02-15 21:48:09 +0100

    modded

--- minecraft/servers/Modded/start.sh
+++ minecraft/servers/Modded/start.sh
@@ -1,17 +1,34 @@
-#!/bin/bash
+#! /bin/bash
 
 cd "$(dirname "$0")"
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 memory_min memory_max"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 server_name memory_min memory_max"
     exit 1
 fi
 
-MEM_MIN=$1
-MEM_MAX=$2
+SERVER_NAME=$1
+MEM_MIN=$2
+MEM_MAX=$3
+auto_restart=false
 
-## Add server start here
-echo "Server not implemented yet!"
-sleep 100
+if [ $# == 4 ]; then
+    auto_restart=$4
+fi
 
+minecraft() {
+    java -Xms${MEM_MIN} -Xmx${MEM_MAX} -jar paper.jar nogui --world-container worlds
+}
+
+while :
+    minecraft
+    $auto_restart
+do
+    if [ -v DEDI_LOGS_CHANNEL_WEBHOOK ]; then
+	curl -H "Content-Type: application/json" -X POST -d "{\"content\": \"$SERVER_NAME stopped. Performing automatic restart...\"}" $DEDI_LOGS_CHANNEL_WEBHOOK
+    else
+        echo "Restarting..."
+    fi
+    sleep 1
+done
 exit 0
