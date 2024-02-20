3cd76cfa84596e0ad900f39878e2bfde1396ac80
orbitaldot <orbitaldotogg@gmail.com>
2024-02-21 00:17:54 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ sudo apt install screen -y
 sudo apt install nodejs -y
 sudo apt install openjdk-17-jre-headless -y
 
-# Currently unused?
-sudo mkdir -p /opt/minecraft/tools/mcrcon
-sudo git clone https://github.com/Tiiffi/mcrcon /opt/minecraft/tools/mcrcon
-
 # ./etc/
 sudo crontab -u $(whoami) etc/crontab
 sudo cp etc/motd /etc/
