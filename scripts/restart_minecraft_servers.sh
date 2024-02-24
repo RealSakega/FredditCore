d6c96454df46808363f316783d8c49209409e401
orbitaldot <orbitaldotogg@gmail.com>
2024-02-24 13:20:18 +0100

    Update restart_minecraft_servers.sh

--- scripts/restart_minecraft_servers.sh
+++ scripts/restart_minecraft_servers.sh
@@ -2,16 +2,16 @@
 
 cd "$(dirname "$0")"
 
-bash ./make_minecraft_servers.sh command "say Server restarting in 5 minutes"
+bash ./make_minecraft_servers.sh command "say Automatic restart in 5 minutes"
 bash ./make_minecraft_servers.sh command "discordsrv bcast Automatic server restart in 5 minutes"
 
 sleep 4m
 
-bash ./make_minecraft_servers.sh command "say Server restarting in 1 minute"
+bash ./make_minecraft_servers.sh command "say Automatic restart in 1 minute"
 
 sleep 1m
 
-bash ./make_minecraft_servers.sh command "say Server restarting now"
+bash ./make_minecraft_servers.sh command "say Automatic restart now"
 bash ./make_minecraft_servers.sh command "stop"
 
 exit 0
