0b1f4c1993a9f7bbee7f5fe45fb017fb6be68db0
orbitaldot <orbitaldotogg@gmail.com>
2024-03-26 21:48:17 +0100

    Update save-freddit.py

--- tools/save-freddit/save-freddit.py
+++ tools/save-freddit/save-freddit.py
@@ -11,26 +11,18 @@ sleep_timer = 6 # should be safe
 
 headers = {'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'}
 
-# Loop through the posts
-for post in posts:
-    # Get the title and url
-    url = post
-
+for url in posts:
     url = url.replace('https://www.reddit.com', 'https://old.reddit.com')
 
-    # Replace any characters that are not allowed in filenames
-    title = url.replace('/', '_').replace(':', '_').replace('?', '_').replace('&', '_').replace('=', '_').replace(' ', '_')
-
-    print(url, title)
-
-    # Get the HTML content of the page
     response = requests.get(url, headers=headers)
     html = response.text
 
     #remove non-ascii characters
     html = ''.join([i if ord(i) < 128 else ' ' for i in html])
 
-    # Write the HTML content to a file
+    # Replace any characters that are not allowed in filenames
+    title = url.replace('/', '_').replace(':', '_').replace('?', '_').replace('&', '_').replace('=', '_').replace(' ', '_')
+
     with open(f'out/{title}.html', 'w') as f:
         f.write(html)
 
