b27269fdd73a6981c00f56a6c313d35de983dc36
orbitaldot <orbitaldotogg@gmail.com>
2024-03-27 12:36:29 +0100

    remove bs4

--- tools/save-freddit/src/collect_posts.py
+++ tools/save-freddit/src/collect_posts.py
@@ -3,7 +3,6 @@ import json
 import time
 import random
 import sys
-from bs4 import BeautifulSoup
 
 sleep_timer = 3 # should be safe
 headers = {'User-Agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'}
